129a5ba0af9709ba0df691245117acad7102a088
Martino888 <maxmartino33@gmail.com>
2022-03-17 16:24:21 +0100

    menu

--- main.js
+++ main.js
@@ -9,6 +9,11 @@
 // Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
 // Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 // Consiglio
+// Milestone 4
+// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+// Milestone 5
+// Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+// Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti
 // Pensate bene a come strutturare i dati prima di implementare il codice.
 
 
@@ -32,6 +37,7 @@ const app = new Vue({
                         text: 'ciao',
                         data: '12/12/2012',
                         inviato: true,
+                        menu: false,
                     },
                     {
                         text: 'prova',
@@ -49,6 +55,7 @@ const app = new Vue({
                     text: 'come stai',
                     data: '12/12/2012',
                     inviato: true,
+                    menu: false,
                 }]
             },
             {
@@ -144,7 +151,11 @@ const app = new Vue({
                     });
                 },1000)
             }
-        }
+        },
+        blockPanel(index) {
+            console.log(this.contacts[this.activeIndex].messaggis[index].menu )
+            this.contacts[this.activeIndex].messaggis[index].menu = !this.contacts[this.activeIndex].messaggis[index].menu 
+        },
     }
 
 });
